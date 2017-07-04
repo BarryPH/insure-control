e29863f51e8a28c925b2561211a4cfc802419955
Callum Flack <callumflack@gmail.com>
2017-07-04 21:17:10 +1000

    client & styling revisions for a first LIVE deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+Made with Harpjs. Deployed via Now. To update, alias the new deployment:
+
+> now alias set <deploymentId> insurecontrol.now.sh
--- public/assets/js/formspree.js
+++ public/assets/js/formspree.js
@@ -5,58 +5,58 @@ let defaultSubmitText = submitButton.textContent;
 
 
 function setFlashMessage(success) {
-	while (flashMessagesContainer.firstChild) {
-		flashMessagesContainer.removeChild(flashMessagesContainer.firstChild);
-	}
+    while (flashMessagesContainer.firstChild) {
+        flashMessagesContainer.removeChild(flashMessagesContainer.firstChild);
+    }
 
-	let messageElem = document.createElement('div');
-	let messageType = success ? 'success' : 'danger';
-	messageElem.classList.add('c-alert');
-	messageElem.classList.add(`c-alert-${messageType}`);
+    let messageElem = document.createElement('div');
+    let messageType = success ? 'success' : 'danger';
+    messageElem.classList.add('c-alert');
+    messageElem.classList.add(`c-alert-${messageType}`);
 
-	if (success) {
-		messageElem.innerHTML = `Thank you for signing up! Download our pdf of the 7 things to can do right now by clicking <a class='o-linkUnderline' href='/download'>here</a>.`;
-	} else {
-		messageElem.innerHTML = 'Woops, something went wrong. Please try again.';
-	}
+    if (success) {
+        messageElem.innerHTML = `Thank you for signing up! Download our pdf of the 7 things to can do right now by clicking <a class='o-linkUnderline' href='assets/docs/insure-control-7-things-you-can-do-right-now.pdf'>here</a>.`;
+    } else {
+        messageElem.innerHTML = 'Woops, something went wrong. Please try again.';
+    }
 
-	flashMessagesContainer.appendChild(messageElem);
+    flashMessagesContainer.appendChild(messageElem);
 }
 
 
 function submitForm(event) {
-	let xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
 
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				let response = JSON.parse(xhr.responseText);
-				setFlashMessage(true);
-			} else {
-				setFlashMessage(false);
-			}
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let response = JSON.parse(xhr.responseText);
+                setFlashMessage(true);
+            } else {
+                setFlashMessage(false);
+            }
 
-			submitButton.removeAttribute('disabled');
-			submitButton.textContent = defaultSubmitText;
-		}
-	};
+            submitButton.removeAttribute('disabled');
+            submitButton.textContent = defaultSubmitText;
+        }
+    };
 
-	let formData = {};
-	[].map.call(contactForm.elements, (element) => {
-		if (element.tagName.toLowerCase() !== 'input') return;
-		formData[element.name] = element.value;
-	});
+    let formData = {};
+    [].map.call(contactForm.elements, (element) => {
+        if (element.tagName.toLowerCase() !== 'input') return;
+        formData[element.name] = element.value;
+    });
 
-	xhr.open('POST', contactForm.action, true);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify(formData));
+    xhr.open('POST', contactForm.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(formData));
 }
 
 
 contactForm.addEventListener('submit', (event) => {
-	event.preventDefault();
+    event.preventDefault();
 
-	submitButton.setAttribute('disabled', 'true');
-	submitButton.textContent = 'Sending...';
-	submitForm();
+    submitButton.setAttribute('disabled', 'true');
+    submitButton.textContent = 'Sending...';
+    submitForm();
 });
